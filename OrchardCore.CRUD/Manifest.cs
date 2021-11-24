4d3cdca6d5992f588eee0c5d26b4841194c9d92d
Szymon Seliga <szymon@onefourseven.pl>
2021-11-24 14:48:47 +0100

    Generalize frontend content display

--- OrchardCore.CRUD/Manifest.cs
+++ OrchardCore.CRUD/Manifest.cs
@@ -2,9 +2,9 @@ using OrchardCore.Modules.Manifest;
 
 [assembly: Module(
     Name = "OrchardCore.CRUD",
-    Author = "Orchard Skills",
+    Author = "Orchard Skills, Onefourseven Labs",
     Website = "https://orchardcore.net",
-    Version = "1.0.0-rc2",
+    Version = "1.1.0",
     Description = "OrchardCore.CRUD",
     Category = "Content Management"
 )]
